92bfc3d0a43ce6aa61df198a3c2b5e4f2d7acd16
wurtel7 <163287792+wurtel7@users.noreply.github.com>
2024-03-20 11:48:50 +0100

    -uziek

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        . . . . .
-        `)
-    basic.pause(1000)
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        `)
+    music.play(music.stringPlayable("F C - C5 E C5 G C5 ", 500), music.PlaybackMode.InBackground)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
@@ -23,14 +9,11 @@ input.onGesture(Gesture.Shake, function () {
         . # # # .
         # . . . #
         `)
-    basic.pause(1000)
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        `)
+    basic.pause(200)
+    leven += -10
+})
+input.onButtonPressed(Button.B, function () {
+    leven += 10
 })
 basic.showLeds(`
     . # . # .
@@ -39,6 +22,43 @@ basic.showLeds(`
     # # # # #
     . . . . .
     `)
+let leven = 60
+loops.everyInterval(1000, function () {
+    leven += -1
+})
 basic.forever(function () {
-	
+    if (leven < 1) {
+        basic.showIcon(IconNames.Skull)
+        basic.pause(2000)
+        game.gameOver()
+    }
+    if (leven < 10) {
+        basic.showIcon(IconNames.Confused)
+    } else if (leven < 30) {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
