7559b5b8e101dd692d880dda16857fe08888e87a
wurtel7 <163287792+wurtel7@users.noreply.github.com>
2024-03-20 10:51:18 +0100

    gezichten

--- main.ts
+++ main.ts
@@ -1,3 +1,44 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+})
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    `)
 basic.forever(function () {
-    basic.showString("xander")
+	
 })
